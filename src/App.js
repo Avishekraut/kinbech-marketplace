962c45e3edccc680f8a173d7ada6405ff3f3f117
Avishek Raut <avishekraut123@gmail.com>
2023-08-31 21:38:49 +0545

    Added categories carousel component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Nav from './components/Nav';
 import './App.css';
+import Hero from './components/Hero';
+import Categories from './components/Categories';
 
 const App = () => {
   return (
     <section>
     <div>
       <Nav />
+      <Hero />
+      <Categories />
     </div>
     </section>
   );
