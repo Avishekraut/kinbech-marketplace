aeee2fd4d20e6cbd64f43935ef4698d9a050e98c
Avishek Raut <avishekraut123@gmail.com>
2023-09-01 08:21:10 +0545

    App.js update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Categories from './components/Categories';
 import FeaturedDisProducts from './components/FeaturedDisProducts';
 import Title from './components/Title';
+import LatestUploads from './components/LatestUploads';
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
       <Categories />
       <Title content="Featured Ads"/>
       <FeaturedDisProducts />
+      <Title content="Latest Uploads"/>
+      <LatestUploads />
     </div>
     </section>
   );
