bd26ef5441ce8f643fb0a609cb4839db89095b2b
Avishek Raut <avishekraut123@gmail.com>
2023-09-03 23:17:55 +0545

    chnaged items to show in mobile in categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -19,11 +19,11 @@ const Categories = () => {
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 2,
-      slidesToSlide: 2, // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1,
+      items: 2,
       slidesToSlide: 1, // optional, default to 1.
     },
   };
