679dcca689ef4aceaeffaa325ba62bc01ee6954a
Avishek Raut <avishekraut123@gmail.com>
2023-09-03 23:18:53 +0545

    Made landing/home page responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BiSearch } from 'react-icons/bi';
+import {AiOutlineMenu} from "react-icons/ai";
 
 const Nav = () => {
   return (
     <div className='nav-bar'>
-      <h1>KinBech</h1>
+      <AiOutlineMenu size={26} className='hamburger-menu'/>
+      <h1 className='logo-text'>KinBech</h1>
       <div className="search">
       <input className='search-box' type='text'name='search' placeholder='What are you looking for?'></input>
       <span className='search-icon'> <BiSearch size={19}/></span>
