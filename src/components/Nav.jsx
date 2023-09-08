f0a77c4a2145c5df6468d89867ad6ea9df3ce5b8
Avishek Raut <avishekraut123@gmail.com>
2023-09-08 09:57:14 +0545

    Added Links to use react router

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { BiSearch } from 'react-icons/bi';
 import {AiOutlineMenu} from "react-icons/ai";
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <div className='nav-bar'>
       <AiOutlineMenu size={26} className='hamburger-menu'/>
-      <h1 className='logo-text'>KinBech</h1>
+      <h1 className='logo-text'><Link to="/">KinBech</Link></h1>
       <div className="search">
       <input className='search-box' type='text'name='search' placeholder='What are you looking for?'></input>
       <span className='search-icon'> <BiSearch size={19}/></span>
       </div>
       <ul className='nav-links'>
-        <li>Login</li>
-        <li>Signup</li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
       </ul>
       <button className='postad-btn'>Post Ad</button>
     </div>
