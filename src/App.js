0efbeb5d11c9df49008da253f1590ad50ea797f2
Avishek Raut <avishekraut123@gmail.com>
2023-09-08 09:58:12 +0545

    added routes and route paths

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Footer from './components/Footer';
 import Landing from './pages/Landing';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
     <section>
-      {/* <Landing/> */}
-      <Signup />
-      {/* <Login /> */}
+      <Routes>
+        <Route path='/' element={<Landing/>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
     </section>
       <Footer />
     </div>
