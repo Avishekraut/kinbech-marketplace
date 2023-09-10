f286042877a4cbaa90b49a67b6b54c00211ac98d
Avishek Raut <avishekraut123@gmail.com>
2023-09-10 22:00:09 +0545

    update in signup

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Nav from "../components/Nav";
-import { FcGoogle } from "react-icons/fc";
 
 const Signup = () => {
+  const onFinish =(values) => {
+    console.log("Success:",values)
+  }
   return (
     <>
       <Nav />
       <div className="flex justify-center my-12">
-        <form
+        <form onFinish={onFinish}
           className="w-[400px] border border-[#e0e3e4] rounded-sm px-7 py-10">
           <h1 className="text-4xl font-medium">Create an account</h1>
           <p className="font-medium text-lg mt-3">Enter your details below </p>
@@ -37,9 +39,7 @@ const Signup = () => {
             />
           </div>
           <div className="mt-6 flex flex-col gap-y-4">
-            <button className="py-2 rounded bg-[#14ae5c] text-white text-lg font-medium hover:bg-[#009951] active:scale-[.98] active:duration-75 transition-all ease-in-out ">
-              Create Account
-            </button>
+            <input type="submit" value="Create Account" className="py-2 rounded bg-[#14ae5c] text-white text-lg font-medium hover:bg-[#009951] active:scale-[.98] active:duration-75 transition-all ease-in-out cursor-pointer"/>
           </div>
           <p className="mt-4 text-center hover:text-slate-600 cursor-pointer">
             Already have account? Log In
