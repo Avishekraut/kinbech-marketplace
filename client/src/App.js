c98cd565d9da7216f292b32040fc2ec2c51d5d3d
Avishek Raut <avishekraut123@gmail.com>
2023-09-25 09:39:59 +0545

    App.js update with protected pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
-import Footer from './components/Footer';
-import Landing from './pages/Landing';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import Footer from "./components/Footer";
+import Landing from "./pages/Landing";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { Route, Routes } from "react-router-dom";
+import ProtectedPage from "./components/ProtectedPage";
 
 const App = () => {
   return (
     <div>
-    <section>
-      <Routes>
-        <Route path='/' element={<Landing/>} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </section>
+      <section>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedPage>
+                <Landing />
+              </ProtectedPage>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </section>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
