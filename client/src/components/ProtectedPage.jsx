7608823e9c063c78422524e7c34c04dec865800a
Avishek Raut <avishekraut123@gmail.com>
2023-09-27 09:39:26 +0545

    added navbar with userprofile and logout options

--- client/src/components/ProtectedPage.jsx
+++ client/src/components/ProtectedPage.jsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { message } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { GetCurrentUser } from "../apicalls/users";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BiUser } from "react-icons/bi";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { TbLogout } from "react-icons/tb";
 
-const ProtectedPage = ({children}) => {
+//UserProfileButton component 
+const UserProfileButton = ({ user }) => {
+  const navigate = useNavigate();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/Login");
+  };
+
+  return (
+    <div className="relative">
+      <div
+        className="bg-white py-2 px-3 rounded flex items-center gap-1 cursor-pointer"
+        onClick={toggleDropdown}
+      >
+        <BiUser size={18} />
+        <span>{user.name}</span>
+        <RiArrowDropDownLine size={22} />
+      </div>
+      {isDropdownOpen && (
+        <div className="absolute right-0 mt-2 w-[170px] bg-white border border-gray-300 rounded shadow-md">
+          <ul className="py-2">
+            <li
+              className="flex items-center justify-between px-4 hover:bg-gray-100 cursor-pointer"
+              onClick={handleLogout}
+            >
+              Logout
+              <TbLogout size={18}/>
+            </li>
+            {/* Add additional dropdown items here */}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const ProtectedPage = ({ children }) => {
   const [user, setUser] = React.useState(null);
   const navigate = useNavigate();
   const validateToken = async () => {
@@ -28,15 +74,18 @@ const ProtectedPage = ({children}) => {
       navigate("/Login");
     }
   }, []);
+
   return (
-    <div>
-      {user && (
-        <div>
-          {user.name}
-          {children}
+    user && (
+      <div>
+        {/* Header */}
+        <div className="flex justify-between items-center p-2 my-3">
+          <h1 className="font-bold text-2xl">KinBech</h1>
+          <UserProfileButton user={user} />
         </div>
-      )}
-    </div>
+        <div>{children}</div>
+      </div>
+    )
   );
 };
 
