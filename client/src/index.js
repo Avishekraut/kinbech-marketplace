516471734f05a9a17482ae3a388433cacd57864f
Avishek Raut <avishekraut123@gmail.com>
2023-10-01 21:47:13 +0545

    added antd theme Config

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,28 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { ConfigProvider } from "antd";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ConfigProvider
+      theme={{
+        components: {
+          Button: {
+            colorPrimary: "#14ae5c",
+            colorPrimaryHover: "#009951",
+            boxShadow: "none",
+          },
+        },
+        token: {
+          colorPrimary: "#14ae5c",
+        },
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ConfigProvider>
   </Provider>
 );
