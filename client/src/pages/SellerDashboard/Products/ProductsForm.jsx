5f6480f5733fad235fcea20ab0edbd57b6e1a453
Avishek Raut <avishekraut123@gmail.com>
2023-10-01 21:22:59 +0545

    added products form

---
+++ client/src/pages/SellerDashboard/Products/ProductsForm.jsx
@@ -0,0 +1,17 @@
+import { Modal } from "antd";
+import React from "react";
+
+function ProductsForm({ showProductForm, setShowProductForm }) {
+  return(
+  <Modal
+    title=""
+    open={showProductForm}
+    onCancel={() => setShowProductForm(false)}
+    centered
+  >
+    <h1>Products Form</h1>
+  </Modal>
+  )
+}
+
+export default ProductsForm;
---
+++ client/src/pages/SellerDashboard/Products/index.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Button } from "antd";
+import ProductsForm from "./ProductsForm";
+
+function Products() {
+  const [showProductForm, setShowProductForm] = useState(false);
+
+  return (
+    <div>
+      <div className="flex justify-end">
+        <Button type="default" onClick={() => setShowProductForm(true)}>
+          Add Product
+        </Button>
+      </div>
+
+      {showProductForm && (
+        <ProductsForm
+          showProductForm={showProductForm}
+          setShowProductForm={setShowProductForm}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Products;
