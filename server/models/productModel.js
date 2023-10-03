ed5e6cd2b569b442859e4b798fc40f36595a90ef
Avishek Raut <avishekraut123@gmail.com>
2023-10-03 09:44:06 +0545

    created product model

---
+++ server/models/productModel.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: "string",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    price: {
+      type: "number",
+      required: true,
+    },
+    category: {
+      type: "string",
+      required: true,
+    },
+    condition: {
+      type: "string",
+      required: true,
+    },
+    images: {
+      type: "array",
+      default: [],
+      required: true,
+    },
+    billAvaiable: {
+      type: "boolean",
+      default: false,
+      required: true,
+    },
+    warrantyAvailable: {
+      type: "boolean",
+      default: false,
+      required: true,
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    status: {
+        type: String,
+        default: "pending",
+        required: true,
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("products", productSchema)
