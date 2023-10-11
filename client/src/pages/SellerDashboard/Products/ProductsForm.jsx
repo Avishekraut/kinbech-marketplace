1e43477bb9ec9fc3909efd99740203a6568e01db
Avishek Raut <avishekraut123@gmail.com>
2023-10-11 21:14:03 +0545

    ProductsForm update for edit product

--- client/src/pages/SellerDashboard/Products/ProductsForm.jsx
+++ client/src/pages/SellerDashboard/Products/ProductsForm.jsx
@@ -10,9 +10,9 @@ import {
   message,
 } from "antd";
 import TextArea from "antd/es/input/TextArea";
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Addproduct } from "../../../apicalls/products";
+import { Addproduct, EditProduct } from "../../../apicalls/products";
 import { setLoader } from "../../../redux/loadersSlice";
 
 const additionalThings = [
@@ -33,31 +33,50 @@ const rules = [
   },
 ];
 
-function ProductsForm({ showProductForm, setShowProductForm }) {
+function ProductsForm({
+  showProductForm,
+  setShowProductForm,
+  selectedProduct,
+  getData,
+}) {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.users);
   const formRef = React.useRef(null);
   const onFinish = async (values) => {
     try {
-      values.seller = user._id;
-      values.status = "pending";
       dispatch(setLoader(true));
-      const response = await Addproduct(values);
-      dispatch(setLoader(false));
-      if (response.success) {
-        message.success(response.message);
-        setShowProductForm(false);
+      let response = null;
+      if (selectedProduct) {
+        response = await EditProduct(selectedProduct._id, values);
       } else {
-        message.error(response.message);
+        values.seller = user._id;
+        values.status = "pending";
+        response = await Addproduct(values); // Ensure response is assigned here
+      }
+
+      dispatch(setLoader(false));
+      if (response && response.success) {
+        message.success(response.message);
+        getData();
+        setShowProductForm(false); // Close the form if the operation is successful
+      } else {
+        message.error(response ? response.message : "An error occurred.");
       }
     } catch (error) {
       dispatch(setLoader(false));
       message.error(error.message);
     }
   };
+
   const handleCancel = () => {
     setShowProductForm(false);
   };
+
+  useEffect(() => {
+    if (selectedProduct) {
+      formRef.current.setFieldsValue(selectedProduct);
+    }
+  }, [selectedProduct]);
 
   return (
     <Modal
@@ -78,74 +97,83 @@ function ProductsForm({ showProductForm, setShowProductForm }) {
         formRef.current.submit();
       }}
     >
-      <Tabs defaultActiveKey="1">
-        <Tabs.TabPane tab="General" key="1">
-          <Form layout="vertical" ref={formRef} onFinish={onFinish}>
-            <Form.Item label="Name" name="name" rules={rules}>
-              <Input type="text" />
-            </Form.Item>
-            <Form.Item label="Description" name="description" rules={rules}>
-              <TextArea type="text" />
-            </Form.Item>
+      <div>
+        <h1 className="text-2xl text-center font-semibold">
+          {selectedProduct ? "Edit Product" : "Add Product"}
+        </h1>
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="General" key="1">
+            <Form layout="vertical" ref={formRef} onFinish={onFinish}>
+              <Form.Item label="Name" name="name" rules={rules}>
+                <Input type="text" />
+              </Form.Item>
+              <Form.Item label="Description" name="description" rules={rules}>
+                <TextArea type="text" />
+              </Form.Item>
 
-            <Row gutter={[16, 16]}>
-              <Col span={8}>
-                <Form.Item label="Price" name="price" rules={rules}>
-                  <Input type="number" />
-                </Form.Item>
-              </Col>
-
-              <Col span={8}>
-                <Form.Item label="Category" name="category" rules={rules}>
-                  <Select>
-                    <option value="Household Electronics">
-                      Household Electronics
-                    </option>
-                    <option value="Car">Car</option>
-                    <option value="Bike">Bike</option>
-                    <option value="Smartphone">Smartphone</option>
-                    <option value="Laptop">Laptop</option>
-                    <option value="Camera">Camera</option>
-                  </Select>
-                </Form.Item>
-              </Col>
-
-              <Col span={8}>
-                <Form.Item label="Condition" name="condition" rules={rules}>
-                  <Select>
-                    <option value="Brand New">Brand New</option>
-                    <option value="Used-Like-New">Used-Like New</option>
-                    <option value="Used-Good">Used-Good</option>
-                  </Select>
-                </Form.Item>
-              </Col>
-            </Row>
-
-            <div className="flex gap-10">
-              {additionalThings.map((item) => {
-                return (
-                  <Form.Item label={item.label} name={item.name} valuePropName="checked">
-                    <Input
-                      type="checkbox"
-                      className="w-8 h-8"
-                      value={item.name}
-                      onChange={(e) => {
-                        formRef.current.setFieldsValue({
-                          [item.name]: e.target.checked,
-                        });
-                      }}
-                      checked={formRef.current?.getFieldsValue(item.name)}
-                    />
+              <Row gutter={[16, 16]}>
+                <Col span={8}>
+                  <Form.Item label="Price" name="price" rules={rules}>
+                    <Input type="number" />
                   </Form.Item>
-                );
-              })}
-            </div>
-          </Form>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Images" key="2">
-          <h1>Images</h1>
-        </Tabs.TabPane>
-      </Tabs>
+                </Col>
+
+                <Col span={8}>
+                  <Form.Item label="Category" name="category" rules={rules}>
+                    <Select>
+                      <option value="Household Electronics">
+                        Household Electronics
+                      </option>
+                      <option value="Car">Car</option>
+                      <option value="Bike">Bike</option>
+                      <option value="Smartphone">Smartphone</option>
+                      <option value="Laptop">Laptop</option>
+                      <option value="Camera">Camera</option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+
+                <Col span={8}>
+                  <Form.Item label="Condition" name="condition" rules={rules}>
+                    <Select>
+                      <option value="Brand New">Brand New</option>
+                      <option value="Used-Like-New">Used-Like New</option>
+                      <option value="Used-Good">Used-Good</option>
+                    </Select>
+                  </Form.Item>
+                </Col>
+              </Row>
+
+              <div className="flex gap-10">
+                {additionalThings.map((item) => {
+                  return (
+                    <Form.Item
+                      label={item.label}
+                      name={item.name}
+                      valuePropName="checked"
+                    >
+                      <Input
+                        type="checkbox"
+                        className="w-8 h-8"
+                        value={item.name}
+                        onChange={(e) => {
+                          formRef.current.setFieldsValue({
+                            [item.name]: e.target.checked,
+                          });
+                        }}
+                        checked={formRef.current?.getFieldsValue(item.name)}
+                      />
+                    </Form.Item>
+                  );
+                })}
+              </div>
+            </Form>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Images" key="2">
+            <h1>Images</h1>
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
     </Modal>
   );
 }
