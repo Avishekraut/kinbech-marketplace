ef42ae06b94f42efd14ad50bf518e8c420178d6c
Avishek Raut <avishekraut123@gmail.com>
2023-11-01 15:55:14 +0545

    image upload frontend

--- client/src/pages/SellerDashboard/Products/Images.jsx
+++ client/src/pages/SellerDashboard/Products/Images.jsx
@@ -1,12 +1,44 @@
-import { Button, Upload } from "antd";
+import { Button, Upload, message } from "antd";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setLoader } from "../../../redux/loadersSlice";
 
-const Images = (selectedProduct, setShowProductForm, getData) => {
+const Images = ({selectedProduct, setShowProductForm, getData}) => {
+  const [file = null, setFile] = React.useState(null);
+  const dispatch = useDispatch();
+  const upload = () => {
+    try {
+      dispatch(setLoader(false));
+      //upload image logic here
+    } catch (error) {
+      dispatch(setLoader(false));
+      message.error(error.message);
+    }
+  };
+
   return (
     <div>
-      <Upload listType="picture" beforeUpload={() => false}>
+      <Upload
+        listType="picture"
+        beforeUpload={() => false}
+        onChange={(info) => setFile(info.file)}
+      >
         <Button type="dashed">Upload Image</Button>
       </Upload>
+
+      <div className="flex justify-end gap-4 mt-4">
+        <Button
+          type="default"
+          onClick={() => {
+            setShowProductForm(false);
+          }}
+        >
+          Cancel
+        </Button>
+        <Button type="default" disabled={!file} onClick={upload}>
+          Upload
+        </Button>
+      </div>
     </div>
   );
 };
