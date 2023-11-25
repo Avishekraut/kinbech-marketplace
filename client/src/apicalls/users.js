5911fd0ecf8f43f6f78ae56becdca328b423ad31
Avishek Raut <avishekraut123@gmail.com>
2023-11-25 10:02:17 +0545

    Add GetAllUsers function for fetching all users

--- client/src/apicalls/users.js
+++ client/src/apicalls/users.js
@@ -21,10 +21,20 @@ export const LoginUser = async (payload) => {
   }
 };
 
-//get user
+//get current user
 export const GetCurrentUser = async () => {
   try {
     const response = await axiosInstance.get("/api/users/get-current-user");
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+//get all users
+export const GetAllUsers = async () => {
+  try {
+    const response = await axiosInstance.get("/api/users/get-users");
     return response.data;
   } catch (error) {
     return error.message;
