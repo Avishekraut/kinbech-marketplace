c4583f11769caebdbe85567e9069d8fa38d33344
Avishek Raut <avishekraut123@gmail.com>
2023-11-30 22:09:23 +0545

    only admin page access update

--- client/src/pages/Admin/index.jsx
+++ client/src/pages/Admin/index.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Tabs } from "antd";
 import Products from "./Products";
 import Users from "./Users";
+import { useSelector } from "react-redux";
+import { useNavigate} from "react-router-dom"
 
 const Admin = () => {
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.users);
+  useEffect(() => {
+    if (user.role !== "admin") {
+      navigate("/");
+    }
+  }, []);
+
   return (
     <div>
       <Tabs>
@@ -11,7 +21,7 @@ const Admin = () => {
           <Products />
         </Tabs.TabPane>
         <Tabs.TabPane tab="Users" key="2">
-         <Users />
+          <Users />
         </Tabs.TabPane>
       </Tabs>
     </div>
