aa1c3f7ca1674ee6a449cffd43890ca67d26cf11
Avishek Raut <avishekraut123@gmail.com>
2023-12-18 09:51:17 +0545

    bidsRoute update

--- server/server.js
+++ server/server.js
@@ -7,9 +7,11 @@ const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/usersRoute"); 
 const productsRoute = require("./routes/productsRoute");
+const bidsRoute = require("./routes/bidsRoute");
 
 app.use("/api/users", usersRoute); 
 app.use("/api/products", productsRoute);
+app.use("/api/bids", bidsRoute);
 
 app.listen(port, () =>
   console.log(`Node/Express Server started on port ${port}`)
