52f1cccf1ff8023267733c627202f092ab2130e4
Avishek Raut <avishekraut123@gmail.com>
2024-01-16 22:43:48 +0545

    Navbar update(onclick)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Kinbechlogo from "../../src/images/kinbechLogo.png";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex justify-between items-center py-2 my-3">
-      <img src={Kinbechlogo} alt="kinbech logo" className="w-36"/>
+      <img src={Kinbechlogo} alt="kinbech logo" className="w-36 cursor-pointer" onClick={() => navigate("/")} />
       <div className="flex gap-6">
         <Link to="/login" className="text-lg">Login</Link>
         <Link to="/signup" className="text-lg">Signup</Link>
