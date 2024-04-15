1014f2b08531211d0bd057e8330e794b770aa67a
Avishek Raut <avishekraut123@gmail.com>
2024-04-15 22:31:56 +0545

    vercel.json update

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ app.use(express.json());
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
-const cors = require("cors");
 const usersRoute = require("./routes/usersRoute");
 const productsRoute = require("./routes/productsRoute");
 const bidsRoute = require("./routes/bidsRoute");
@@ -14,13 +13,6 @@ app.use("/api/users", usersRoute);
 app.use("/api/products", productsRoute);
 app.use("/api/bids", bidsRoute);
 app.use("/api/notifications", notificationsRoute);
-app.use(
-  cors({
-    origin: ["https://deploy-mern-1whq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
 
 app.listen(port, () =>
   console.log(`Node/Express Server started on port ${port}`)
